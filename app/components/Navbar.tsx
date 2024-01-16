de6e2563e862069fdabc27bb23a807f9b83135bc
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2024-01-16 13:21:46 +0500

    DOJO

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 function Navbar() {
   return (
     <nav>
-      <h1>Dojo Help Desk</h1>
+      <h1>DOJO Help Desk</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
     </nav>
