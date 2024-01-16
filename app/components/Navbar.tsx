4fc15096b7bc0e543a83f8977e518e05147248a5
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2024-01-16 15:27:31 +0500

    Logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,11 @@
+import Image from "next/image";
 import Link from "next/link";
+import Logo from "../../public/dojo-logo.png";
 
 function Navbar() {
   return (
     <nav>
+      <Image src={Logo} alt="Dojo Help Desk Logo" width={70} />
       <h1>DOJO Help Desk</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
