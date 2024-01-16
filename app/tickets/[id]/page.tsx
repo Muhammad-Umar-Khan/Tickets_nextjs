8544a28a25960fe659afe95283e95b50ed8bacd0
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2024-01-16 19:28:29 +0500

    another way for params

--- app/tickets/[id]/page.tsx
+++ app/tickets/[id]/page.tsx
@@ -1,5 +1,11 @@
-function TicketDetails() {
-  return <p>Individual Ticket details</p>;
+// interface TicketDetailsProps {
+//   params: {
+//     id: number;
+//   };
+// }
+function TicketDetails({ params }: { params: { id: number } }) {
+  const { id } = params;
+  return <p>{id}</p>;
 }
 
 export default TicketDetails;
