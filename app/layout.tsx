dfbb67255db45ff4b53f121bb549746dece148d7
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2024-01-16 11:27:30 +0500

    styles, fonts and images

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Rubik } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+const rubik = Rubik({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Tickets",
@@ -17,8 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <h1>Dojo Help Desk</h1>
+      <body className={rubik.className}>
         <Navbar />
         {children}
       </body>
