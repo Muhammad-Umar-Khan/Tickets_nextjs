773a406244690773cf8a44206ee2c8ae1797bab7
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2024-01-16 16:16:52 +0500

    successfully rendered posts/tickets

--- app/tickets/TicketsList.tsx
+++ app/tickets/TicketsList.tsx
@@ -1,10 +1,26 @@
+import { Fragment } from "react";
+
 const getTicketsList = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
   return res.json();
 };
 const TicketsList = async () => {
   const posts = await getTicketsList();
-  return <h1>All Tickets list</h1>;
+  return (
+    <Fragment>
+      {posts.map(
+        (post: { userId: number; id: number; title: string; body: string }) => (
+          <div key={post.id} className="card my-5">
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+            <div className="pill low">
+              <p>Low priority</p>
+            </div>
+          </div>
+        )
+      )}
+    </Fragment>
+  );
 };
 
 export default TicketsList;
