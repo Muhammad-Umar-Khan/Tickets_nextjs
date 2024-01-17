d25901d78200e5b02cf4c2391edbc76ed13e39dd
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2024-01-17 15:48:05 +0500

    lesson 07

--- app/tickets/[id]/page.tsx
+++ app/tickets/[id]/page.tsx
@@ -3,9 +3,32 @@
 //     id: number;
 //   };
 // }
-function TicketDetails({ params }: { params: { id: number } }) {
+//react-query
+const getPostById = async (p_id: number) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${p_id}`,
+    { next: { revalidate: 60 } }
+  );
+  return res.json();
+};
+async function TicketDetails({ params }: { params: { id: number } }) {
   const { id } = params;
-  return <p>{id}</p>;
+  const post = await getPostById(id);
+  return (
+    <main>
+      <nav>
+        <h2>Ticket Details</h2>
+      </nav>
+      <div className="card">
+        <h3>{post.title}</h3>
+        <p>
+          <small>Created by </small>
+          {post.userId}
+        </p>
+        <p>{post.body}</p>
+      </div>
+    </main>
+  );
 }
 
 export default TicketDetails;
