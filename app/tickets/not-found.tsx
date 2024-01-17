aa86e81a0dc5be6374457de05a76e237ce837d29
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2024-01-17 16:59:40 +0500

    specific not-found.tsx not working

--- app/tickets/not-found.tsx
+++ app/tickets/not-found.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 function NotFound() {
   return (
     <main className="text-center">
-      <h2 className="text-3xl">We hit a brick wall.</h2>
+      <h2>We hit a brick wall.</h2>
       <p>The Ticket you are looking for could not be found</p>
       <p>
-        Go back to all <Link href="/">dashboard</Link>
+        Go back to all <Link href="/tickets">tickets</Link>
       </p>
     </main>
   );
